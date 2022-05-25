04f6b7f09025fb46c597ad02bc6e301fe4f35a57
taekwan <techigh.b@gmail.com>
2022-05-25 17:24:31 +0900

    Java OOP #6 JVM Flow

---
+++ src/CallStackTest2.java
@@ -0,0 +1,19 @@
+public class CallStackTest2 {
+    public static void main(String[] args) {
+        System.out.println("main(String[] args) started.");
+        firstMethod();
+        System.out.println("main(String[] args) done.");
+    }
+
+    static void firstMethod() {
+        System.out.println("firstMethod() started.");
+        secondMethod();
+        System.out.println("firstMethod() done.");
+    }
+
+    static void secondMethod() {
+        System.out.println("secondMethod() started.");
+        System.out.println("secondMethod() done.");
+
+    }
+}

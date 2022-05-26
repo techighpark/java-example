6d5248d5b493730ce9851c05995915ec21bb1c60
taekwan <techigh.b@gmail.com>
2022-05-26 11:55:25 +0900

    Java OOP #12 Constructor

---
+++ src/ConstructorTest.java
@@ -0,0 +1,25 @@
+class Data1 {
+    int value;
+}
+
+class Data2 {
+    int value;
+
+    Data2(int x) {
+        value = x;
+    }
+
+    Data2() {
+    }
+
+    
+}
+
+public class ConstructorTest {
+    public static void main(String[] args) {
+        Data1 d1 = new Data1();
+        // Data2 d2 = new Data2(); >> Error -> 기본 생성자 없음 -> constructor 만들어줘야함
+        Data2 d21 = new Data2(1);
+        Data2 d22 = new Data2(); // Error 없음
+    }
+}

037a50e4ca77bdb5b6e34466f88e1d83e6d3a328
taekwan <techigh.b@gmail.com>
2022-05-27 17:14:37 +0900

    Java #7 OOP2 6 Super-SuperTest2

---
+++ src/ch7/SuperTest2.java
@@ -0,0 +1,24 @@
+package ch7;
+
+public class SuperTest2 {
+    public static void main(String[] args) {
+        Child2 c = new Child2();
+        c.method();
+    }
+}
+
+class Parent2 {
+    int x = 10;
+}
+
+class Child2 extends Parent2 {
+    int x = 20;
+
+    void method() {
+        System.out.println(x);
+        System.out.println(this.x);
+        System.out.println(super.x);
+    }
+}
+
+

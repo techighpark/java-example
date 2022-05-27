562068b1c4370ddf980fb9b5d9dd25e7845ce99b
taekwan <techigh.b@gmail.com>
2022-05-27 14:38:47 +0900

    Java OOP #19 Exercise

---
+++ src/Chaper6_7.java
@@ -0,0 +1,30 @@
+public class Chaper6_7 {
+    public static void main(String[] args) {
+        MyPoint p = new MyPoint(5, 4);
+        System.out.println(p);
+    }
+
+}
+
+class MyPoint {
+    int x;
+    int y;
+
+    MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    double getDistance(int x1, int y1) {
+        int dx = (x - x1) * (x - x1);
+        int dy = (y - y1) * (y - y1);
+        return Math.sqrt(dx + dy);
+    }
+
+    public static void main(String[] args) {
+        MyPoint p = new MyPoint(1, 1);
+        System.out.println(p.getDistance(2, 2));
+    }
+
+}

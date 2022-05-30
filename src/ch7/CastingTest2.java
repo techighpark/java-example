dac6b5b8480854d4b6fd3fa0abe989e57e33ea06
taekwan <techigh.b@gmail.com>
2022-05-30 16:18:46 +0900

    Java #7 OOP2 14-2 CastingTest2

--- src/ch7/CastingTest2.java
+++ src/ch7/CastingTest2.java
@@ -4,13 +4,13 @@ public class CastingTest2 {
     public static void main(String[] args) {
         Car car = new Car();
         Car car2 = null;
-        FireEngine fe = null;
+        FireEngine fe;
 
-/*        car.drive();
-        fe = (FireEngine) car;
+        car.drive();
+//        fe = (FireEngine) car;
 //        fe.drive();
 //        car2.drive();
-        car2 = fe;
-        car2.drive();*/
+//        car2 = fe;
+//        car2.drive();
     }
 }
